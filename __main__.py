d9c47c0be513a4b013165af6f95ffe9d1d55a693
Curtis Heinzl <curtis.heinzl@gmail.com>
2022-01-15 19:34:13 -0500

    Node class: removed information hiding

--- __main__.py
+++ __main__.py
@@ -2,7 +2,8 @@
 import linked_list
 
 def main():
-    pass
+    head = linked_list.Node('First node')
+    print('value: ', head.value, '\nnext: ', head.next)
 
 if __name__ == '__main__':
     main()
--- linked_list.py
+++ linked_list.py
@@ -1,26 +1,8 @@
 class LinkedList:
-    def __init__(self):
+    def __init__(self, len=1):
         pass
 
 class Node:
-    def __init__(self, val):
-        self.__val = val
-        self.__next = None
-
-    @property
-    def val(self):
-        return self.__val
-
-    @property
-    def next(self):
-        return self.__next
-
-    @val.setter
-    def val(self, val):
-        self.__var = val
-
-    @val.setter
-    def next(self, next):
-        if type(next) != Node:
-            raise TypeError('must be of type Node')
-        self.__next = next
+    def __init__(self, value='', next=None):
+        self.value = value
+        self.next = next
